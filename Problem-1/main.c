af3d5e87c180e3d3e78c252f06c5cd08e883044d
serhii-kostiuchok <59771229+serhii-kostiuchok@users.noreply.github.com>
2021-01-27 16:54:15 +0200

    Update main.c

--- Problem-1/main.c
+++ Problem-1/main.c
@@ -2,22 +2,8 @@
 
 int main()
 {
-<<<<<<< HEAD
-    char s[12], *p = gets(s);
-
-    while (*p == ' ')
-        p++;
-
+    char s[12], *p = fgets(s, 12, stdin);
     printf("%c %c", *p, *(p + 1));
 
     return 0;
 }
-=======
-	int a;
-
-	if ((scanf("%d", &a) == 1) && (a >= 10) && (a <= 99))
-		printf("%d %d\n", a / 10, a % 10);
-
-	return 0;
-}
->>>>>>> parent of 77b680b... Внесення змін до першого завдання...
